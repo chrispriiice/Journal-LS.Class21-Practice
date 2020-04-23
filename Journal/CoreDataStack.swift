66422dde7fc14b8113ff01621a250c3ef700c65e
Chris Price <christopher.r.price@gmail.com>
2020-04-22 21:53:59 -0400

    Rough draft of autolayout constraints for the Create screen.

---
+++ Journal/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Journal
+//
+//  Created by Chris Price on 4/22/20.
+//  Copyright © 2020 com.chrispriiice. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/Entry+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Chris Price on 4/22/20.
+//  Copyright © 2020 com.chrispriiice. All rights reserved.
+//
+
+import Foundation

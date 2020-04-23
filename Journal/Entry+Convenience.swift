8ae7c3f10f50fe61da7726bc36530c95a52142d7
Chris Price <christopher.r.price@gmail.com>
2020-04-22 22:50:39 -0400

    Set up the Entry extension an intializer.

--- Journal/Entry+Convenience.swift
+++ Journal/Entry+Convenience.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import CoreData
+
+extension Entry {
+    @discardableResult convenience init(title: String, bodyText: String, timestamp: Date, identifier: String, context: NSManagedObjectContext) {
+
+        self.init(context: context)
+        
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
--- Journal/EntryTableViewCell.swift
+++ Journal/EntryTableViewCell.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class EntryTableViewCell: UITableViewCell {
+    
+    
+    
+    var entry: Entry? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }

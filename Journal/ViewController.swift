bc25d12a27ae6e13232a3a3b80e9591e5b36abc6
Chris Price <christopher.r.price@gmail.com>
2020-04-22 21:20:53 -0400

    Initial commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Chris Price on 4/22/20.
+//  Copyright © 2020 com.chrispriiice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
